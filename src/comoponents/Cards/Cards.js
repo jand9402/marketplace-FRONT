20a23aeff952057074cadcb02162d79704e59241
Your Name <you@example.com>
2022-05-21 12:36:52 -0300

    salió pag común, con más tiempo hago lo de las flechas

--- src/comoponents/Cards/Cards.js
+++ src/comoponents/Cards/Cards.js
@@ -4,67 +4,52 @@ import { useSelector, useDispatch } from "react-redux";
 import {Link} from "react-router-dom";
 import Card from "./card"
 import { getCategorys, getProducts} from "../../redux/actions";
+import Paginado from "../Paginado/Paginado";
 
 
 export default function Cards(){
 
-    // const allProducts = useSelector(state => state.products)
-    // console.log(allProducts)
-    // console.log(allProducts)
- const dispatch = useDispatch()
-//     const [pagActual, setPagActual] = useState(1)
-//     const [cardsPorPag, setCardPorPag] = useState(5)
-//     const indiceDeCardsFinal = pagActual * cardsPorPag
-//     const indiceDeCardsPrinc = indiceDeCardsFinal - cardsPorPag
-//     const tarjetasAct = allProducts?.slice( indiceDeCardsPrinc,indiceDeCardsFinal)
+const dispatch = useDispatch()
+const allProducts = useSelector(state => state.products)
+const [currentPage, setCurrentPage] = useState(1)
+const [productsPerPage, setProductsPerPage] = useState(5)
+const indexOfLastCard = currentPage * productsPerPage
+const indexOfFirstCard = indexOfLastCard - productsPerPage
+const currentCards = allProducts.slice(indexOfFirstCard,indexOfLastCard)
 
-    
-// const paginado = (numeroDePag) => {
-//     setPagActual(numeroDePag)
-// }
-const [currentPage, setCurrentPage] = useState(0)
-
-    const [numberPage, setNumberPage] = useState(1)
-
-    const allProducts = useSelector(state => state.products)
-    
-    const [offset, setOffset] = useState(5)
-
-  
-    const filteredCountries = () =>{
-        
-        return allProducts.slice(currentPage,currentPage+offset)
-    }
-
-    const handleNextPage = () =>{
-        if(allProducts.length > currentPage + offset){
-            setCurrentPage(currentPage+offset)
-        }
-        if(allProducts.length>1){
-            setNumberPage(numberPage+1) 
-        }
-    }
-
-    const handlePrevPage= () =>{    
-       if(currentPage>0){
-           setCurrentPage(currentPage-offset)  
-           setNumberPage(numberPage-1)      
-       }  
-       if(currentPage>0){
-        setCurrentPage(currentPage-(offset-1))
-        setNumberPage(numberPage-1)      
-    }
+const paginado = (pageNumber) => {
+    setCurrentPage(pageNumber)
 }
 
-// useEffect (()=> {
-//     currentPage(1)
-// },[filteredCountries])
+//     const handleNextPage = () =>{
+//         if(allProducts.length > currentPage + cardsPerPage){
+//             setCurrentPage(currentPage+cardsPerPage)
+//         }
+//         if(allProducts.length>1){
+//             setNumberPage(numberPage+1) 
+//         }
+//     }
+
+//     const handlePrevPage= () =>{    
+//        if(currentPage >0){
+//            setCurrentPage(currentPage-cardsPerPage)  
+//            setNumberPage(numberPage-1)      
+//        }  
+//        if(currentPage>0){
+//         setCurrentPage(currentPage-(cardsPerPage-1))
+//         setNumberPage(numberPage-1)      
+//     }
+// }
+
+useEffect (()=> {
+    paginado (1)
+},[allProducts])
 
 
-    useEffect (()=> {
-        dispatch(getProducts())
-        dispatch(getCategorys())
-    }, [dispatch])
+useEffect (()=> {
+    dispatch(getProducts())
+    dispatch(getCategorys())
+}, [dispatch])
     
 
     return (
@@ -72,24 +57,21 @@ const [currentPage, setCurrentPage] = useState(0)
             <div className="categoriasHome" >Categorías</div>
             <div className="ordenCards">
 
-                { filteredCountries().map((p)=> {
+                { currentCards.map((p)=> {
                     return(
                         <div key={p._id}>
-                        
-                           <Card id={p._id} image={p.image} name={p.name} price={p.price} />
-                       
-
-                    </div>
-                )} 
-                )}
-                
+                            <Card id={p._id} image={p.image} name={p.name} price={p.price} />
+                        </div> 
+                    )} 
+                )}                
             </div>
-            <div>
+            {/* <div>
             {allProducts.length && <div className="ordenNextPrev">
                 <div  onClick={handlePrevPage}><button className="buttonPrevCard"></button></div>
                 <div onClick={handleNextPage}><button className="buttonNextCard"></button></div>
             </div>}
-           </div>    
+           </div>   */}
+           <Paginado productsPerPage={productsPerPage}  allProducts={allProducts.length} paginado = {paginado}/>  
         </div>
     )
 
