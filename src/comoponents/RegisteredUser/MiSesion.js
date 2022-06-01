662f99653494c293d8454cb768c4a1bae7c73770
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-06-01 03:26:43 -0500

    renderizado ordenes usuario

--- src/comoponents/RegisteredUser/MiSesion.js
+++ src/comoponents/RegisteredUser/MiSesion.js
@@ -1,14 +1,55 @@
-import React from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getOrderDetailByUser } from "../../redux/actions/index";
 import NavBaRSesionUsers from "../NavBar/navBarSesionUsers";
 import './miSesion.css'
+export default function MiSesion() {
+  const user = JSON.parse(localStorage.getItem("userData"));
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.orderDetail);
+  useEffect(() => {
+    dispatch(getOrderDetailByUser());
+  }, [dispatch]);
 
-export default function MiSesion (){
-    return(
-        <div>
-            <NavBaRSesionUsers/>
-            <div className="contenedorSesionUser">
-            soy la sesión del usuario, tengo el registro mis compras
-            </div>
-        </div>
-    )
+  return (
+    <div>
+        <NavBaRSesionUsers/>
+      <div>
+        <h1>Tus Compras</h1>
+        {!data ? (
+          <p>Cargando...</p>
+        ) : (
+          <>
+            {data ? (
+              <>
+                <p>Usuario: {user.name}</p>
+                {data.userOrder?.map((item) => (
+                  <div key={item._id}>
+                    {item.isPaid ? <p>Pagado: SI</p> : <p>Pagado: NO</p>}
+                    {item.isDelivered ? <p>Enviado: SI</p> : <p>Enviado: NO</p>}
+
+                    <h3>Productos</h3>
+                    {item.orderProducts.map((product) => (
+                      <div key={product._id}>
+                        <p>Nombre: {product.name}</p>
+                        <img
+                          src={product.image[0]}
+                          height="150px"
+                          alt="ProductImg"
+                        />
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </>
+            ) : (
+              <p>No encontramos una orden</p>
+            )}
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
+
+
