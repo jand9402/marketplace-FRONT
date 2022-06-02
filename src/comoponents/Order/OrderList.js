5840d55b0341590593c1be0d6078a8314ca28bdb
Jairo Naranjo <jairo_naranjo80111@hotmail.com>
2022-06-01 21:33:05 -0500

    estilos de orden detail, y procesador de pagos

--- src/comoponents/Order/OrderList.js
+++ src/comoponents/Order/OrderList.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { getOrderDetailByUser } from "../../redux/actions/index";
 import StripeCheckout from "react-stripe-checkout";
 import axios from "axios";
+import './Order.css'
 
 export default function CreateOrder() {
   const KEY = process.env.REACT_APP_STRIPE_KEY;
@@ -48,8 +49,13 @@ export default function CreateOrder() {
   }, [stripeToken, history, data?.userOrder[0].totalPrice]);
 
   return (
-    <div>
-      <h1>Lista de tus ordenes</h1>
+    <div className="container mt-5">
+      <div className="d-flex">
+      <h1 className="font">Lista de tus ordenes</h1>
+      <Link to="/PostOrder">
+        <button>Regresar</button>
+      </Link>
+      </div>
       {!data ? (
         <p>Cargando...</p>
       ) : (
@@ -57,25 +63,31 @@ export default function CreateOrder() {
           {data ? (
             <>
               {data.userOrder?.map((item) => (
-                <div key={item._id}>
+                <div  key={item._id}>
                   <h3>Tus productos</h3>
                   {item.orderProducts.map((product) => (
-                    <div key={product._id}>
+                    <div className="row row_lista_ordenes" key={product._id}>
+                      <div className="col">
                       <p>Nombre: {product.name}</p>
                       <img
                         src={product.image[0]}
                         height="150px"
                         alt="ProductImg"
                       />
+                      </div>
+                      <div className="col">
                       <p>Precio: {product.price}</p>
                       <p>Catidad: {product.quantity}</p>
+                      </div>
                     </div>
                   ))}
-                  <h3>Tus datos</h3>
-                  <p>Nombre: {item.deliveryAddress.fullName}</p>
-                  <strong>Total: ${item.totalPrice}</strong>
-                </div>
+                  
+                  <h3 className="font">Tus datos</h3>
+                  <p  className="font">Nombre: {item.deliveryAddress.fullName}</p>
+                  <strong  className="font">Total: ${item.totalPrice}</strong>
+                  </div>
               ))}
+              
               <StripeCheckout
                 name="StoreCel"
                 image="https://cdn-icons-png.flaticon.com/512/2097/2097276.png"
@@ -84,7 +96,7 @@ export default function CreateOrder() {
                 token={onToken}
                 stripeKey={KEY}
               >
-                <button>Pagar</button>
+                <button  className="font">Pagar</button>
               </StripeCheckout>
             </>
           ) : (
--- src/comoponents/Order/Success.js
+++ src/comoponents/Order/Success.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getOrderDetailByUser } from "../../redux/actions/index";
+import { Link } from "react-router-dom";
 
 export default function CreateOrder() {
   const user = JSON.parse(localStorage.getItem("userData"));
@@ -12,24 +13,34 @@ export default function CreateOrder() {
 
   return (
     <div>
-      <div>
-        <h1>Tus Compras</h1>
+      <div className="container">
+        <div className="d-flex">
+        <h1 className="tus_compras font">Tus Compras</h1>
+        <Link to="/">
+        <button>Volver al Home</button>
+        </Link>
+        </div>
         {!data ? (
           <p>Cargando...</p>
         ) : (
-          <>
+          <div>
             {data ? (
               <>
-                <p>Usuario: {user.name}</p>
+              <div></div>
+                <p className="font">Usuario: {user.name}</p>
+                <div  className="container container_orders">
+                  
                 {data.userOrder?.map((item) => (
                   <div key={item._id}>
-                    {item.isPaid ? <p>Pagado: SI</p> : <p>Pagado: NO</p>}
-                    {item.isDelivered ? <p>Enviado: SI</p> : <p>Enviado: NO</p>}
-
-                    <h3>Productos</h3>
+                    <div className="row row_otra">
+                    {item.isPaid ? <p className="col pagado_order font">Pagado: SI</p> : <p className="col Spagado_order font">Pagado: NO</p>}
+                    {item.isDelivered ? <p className="col font">Enviado: SI</p> : <p  className="col font">Enviado: NO</p>}
+                    </div>
+                    <div className="row row_productos_order">
+                    <h3 className="font">Productos</h3>
                     {item.orderProducts.map((product) => (
-                      <div key={product._id}>
-                        <p>Nombre: {product.name}</p>
+                      <div className="col col_orders_user" key={product._id}>
+                        <p className="font">Nombre: {product.name}</p>
                         <img
                           src={product.image[0]}
                           height="150px"
@@ -37,13 +48,17 @@ export default function CreateOrder() {
                         />
                       </div>
                     ))}
-                  </div>
+                    </div>
+                    </div>
+                  
                 ))}
+                
+                </div>
               </>
             ) : (
               <p>No encontramos una orden</p>
             )}
-          </>
+          </div>
         )}
       </div>
     </div>
